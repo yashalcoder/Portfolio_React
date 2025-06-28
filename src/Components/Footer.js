a608407ee4f38f8f9d6eadc429c3788dadda504f
yashalcoder <yashalrafique@gmail.com>
2025-06-28 23:23:40 +0500

    final

--- src/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div className='m-4 '>
-      <h1 className='text-black dark:text-gray-300 text-center  '>@Software by <span className='text-blue-600 font-bold'>Yashal Rafique 😊</span></h1>
-    </div>
-  )
-}
